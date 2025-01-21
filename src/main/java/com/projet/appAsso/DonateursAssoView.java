d3444c59e6691531b97b5129c719ad3820743e82
Gautier Ternisien <gautier.ternisien@universite-paris-saclay.fr>
2025-01-21 23:24:05 +0100

    Tous les menus sont terminés, toutes les apps sont terminées, + suppression des warnings lors de changements de fenetres

--- src/main/java/com/projet/appAsso/DonateursAssoView.java
+++ src/main/java/com/projet/appAsso/DonateursAssoView.java
@@ -1,4 +1,138 @@
 package com.projet.appAsso;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DonateursAssoView {
+
+    @FXML
+    private Label nom_asso;
+
+    @FXML
+    private Button refresh;
+
+    @FXML
+    private Button deconnecter;
+
+    @FXML
+    private Button retour;
+
+    @FXML
+    private ListView list;
+
+    @FXML
+    private Button virer;
+
+    @FXML
+    private Button ajouter;
+
+
+    @FXML
+    public void initialize() {
+        nom_asso.setText(InitialisationAppAsso.associationActuelle.toString());
+
+        deconnecter.setOnMouseClicked(event -> {
+            System.out.println("Bouton 'Se déconnecter' cliqué");
+            Stage stage = (Stage) deconnecter.getScene().getWindow();
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(AppAsso.class.getResource("/com.projet.appAsso/asso_connexion.fxml"));
+                fxmlLoader.setController(new ConnexionAssoView());
+                Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+                stage.setScene(scene);
+                stage.setTitle("Application Association");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        refresh.setOnMouseClicked(event -> {
+            System.out.println("Bouton 'Refresh' cliqué");
+        });
+
+        retour.setOnMouseClicked(event -> {
+            System.out.println("Bouton 'Retour' cliqué");
+            Stage stage = (Stage) retour.getScene().getWindow();
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(AppAsso.class.getResource("/com.projet.appAsso/asso_association.fxml"));
+                fxmlLoader.setController(new AssociationAssoView());
+                Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+                stage.setScene(scene);
+                stage.setTitle("Application Association");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        ajouter.setOnMouseClicked(event -> {
+            System.out.println("Bouton 'Ajouter un donateur' cliqué");
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Ajout d'un donateur");
+            alert.setHeaderText("Quelle donateur voulez vous ajouter ?");
+            alert.setContentText("Choisissez une personne dans la liste suivante :");
+            alert.setResizable(false);
+
+            ComboBox<String> comboBox = new ComboBox<>();
+
+            //comboBox.getItems().addAll(personnes sans asso);
+
+            comboBox.setPromptText("(Choisissez une personne)");
+            comboBox.setStyle(
+                    "-fx-background-color: rgba(237, 237, 237, 1);" +
+                            "-fx-border-radius: 20;" +
+                            "-fx-background-radius: 20;" +
+                            "-fx-border-color: black;"
+            );
+
+            VBox content = new VBox(10, new Label("Donateur :"), comboBox);
+            content.setStyle("-fx-padding: 10;");
+            alert.getDialogPane().setContent(content);
+            ButtonType okButton = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
+            ButtonType cancelButton = new ButtonType("Annuler", ButtonBar.ButtonData.CANCEL_CLOSE);
+            alert.getButtonTypes().setAll(okButton, cancelButton);
+
+            alert.showAndWait().ifPresent(response -> {
+                if (response == okButton) {
+                    String selectedAssociation = comboBox.getValue();
+                    if (selectedAssociation != null) {
+                        System.out.println("Donateur ajouté : " + selectedAssociation);
+                    } else {
+                        System.out.println("Aucun donateur sélectionné !");
+                    }
+                } else {
+                    System.out.println("Ajout annulé.");
+                }
+            });
+        });
+
+        virer.setOnMouseClicked(event -> {
+            System.out.println("Bouton 'Supprimer un donateur' cliqué");
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Supprimer un donateur");
+            alert.setHeaderText("Êtes-vous sûr de vouloir supprimer ce donateur ?");
+            alert.setContentText("");
+            ButtonType buttonTypeYes = new ButtonType("Oui");
+            ButtonType buttonTypeNo = new ButtonType("Non");
+            alert.getButtonTypes().setAll(buttonTypeYes, buttonTypeNo);
+            alert.showAndWait().ifPresent(buttonType -> {
+
+                //EN GROS LE BOUTON EST GRISE TANT QUE RIEN N'EST SELECTIONNE
+
+                if (buttonType == buttonTypeYes) { // AND MEMBER SELECTED , CAR ON NE PEUT PAS VIRER UN MEMBRE SANS LE SELECTIONNER
+                    System.out.println("Donateur supprimé");
+
+                    //EXCLURE LE MEMBRE
+
+                } else {
+                    System.out.println("Annulation de la suppression.");
+                }
+            });
+        });
+    }
+
 }
--- src/main/java/com/projet/appAsso/FacturesNonRegleesAssoView.java
+++ src/main/java/com/projet/appAsso/FacturesNonRegleesAssoView.java
@@ -1,4 +1,68 @@
 package com.projet.appAsso;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class FacturesNonRegleesAssoView {
+    @FXML
+    private Label nom_asso;
+
+    @FXML
+    private Button refresh;
+
+    @FXML
+    private Button deconnecter;
+
+    @FXML
+    private Button retour;
+
+    @FXML
+    private Button regler;
+
+    @FXML
+    public void initialize() {
+        nom_asso.setText(InitialisationAppAsso.associationActuelle.toString());
+
+        deconnecter.setOnMouseClicked(event -> {
+            System.out.println("Bouton 'Se déconnecter' cliqué");
+            Stage stage = (Stage) deconnecter.getScene().getWindow();
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(AppAsso.class.getResource("/com.projet.appAsso/asso_connexion.fxml"));
+                fxmlLoader.setController(new ConnexionAssoView());
+                Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+                stage.setScene(scene);
+                stage.setTitle("Application Association");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        refresh.setOnMouseClicked(event -> {
+            System.out.println("Bouton 'Refresh' cliqué");
+        });
+
+        retour.setOnMouseClicked(event -> {
+            System.out.println("Bouton 'Retour' cliqué");
+            Stage stage = (Stage) retour.getScene().getWindow();
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(AppAsso.class.getResource("/com.projet.appAsso/asso_factures.fxml"));
+                fxmlLoader.setController(new FacturesAssoView());
+                Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+                stage.setScene(scene);
+                stage.setTitle("Application Association");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        regler.setOnMouseClicked(event -> {
+            System.out.println("Bouton 'Régler' cliqué");
+        });
+    };
 }
